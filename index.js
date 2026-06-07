eafa7beafa7b2b4d1666fd934054e1c4357a9126
Aryan Nandanwar <aryannandanwar738@gmail.com>
2026-06-07 19:22:21 +0530

    chore: initialize package.json with express server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "server is healthy" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
