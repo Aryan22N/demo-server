9d08db40be7d247c7c32da41577b07ba7cf9443c
Aryan Nandanwar <aryannandanwar738@gmail.com>
2026-06-08 00:00:07 +0530

    chore: add status field to health check response simple CICD work successed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "server is healthy IN V-3.1" });
+  return res.json({ message: "server is healthy IN V-3.1", status: "success" });
 });
 
 app.listen(PORT, () => {
